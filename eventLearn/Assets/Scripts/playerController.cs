0c5cce76b894ee4e94ac202737232fe8ddc97269
LuWangThreekit <L236wang@gmail.com>
2019-10-10 00:05:53 -0400

    events

---
+++ eventLearn/Assets/Scripts/playerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody rb;
+  
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizatal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizatal,0,moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
